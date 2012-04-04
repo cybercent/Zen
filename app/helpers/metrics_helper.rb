c0b5769e12af6a3768c5bcf2802a4a5db750f029
Daniel Podaru <dan@herewedate.com>
2012-04-04 18:38:01 +0200

    Converted sum results from metrics query to float as PostrgeSQL returned strings

--- app/helpers/metrics_helper.rb
+++ app/helpers/metrics_helper.rb
@@ -5,18 +5,18 @@ module MetricsHelper
   end
   
   def value_per_state_value_list(values)
-    values.map{|a| rounded_value(a[1]) if a[1] > 0}.compact.join(',')
+    values.map{|a| rounded_value(a[1].to_f) if a[1].to_f > 0}.compact.join(',')
   end
   
   def value_per_state_legend_list(values)
-    raw values.map{|a| "\"#{a[0]}: #{rounded_value(a[1])} (%%.%%)\"" if a[1] > 0}.compact.join(',')
+    raw values.map{|a| "\"#{a[0]}: #{rounded_value(a[1].to_f)} (%%.%%)\"" if a[1].to_f > 0}.compact.join(',')
   end
   
   def time_per_state_value_list(values)
-    values.map{|a| rounded_value(a[1]) if a[1] > 0 && !['won', 'lost', 'hold'].include?(a[0])}.compact.join(',')
+    values.map{|a| rounded_value(a[1].to_f) if a[1].to_f > 0 && !['won', 'lost', 'hold'].include?(a[0])}.compact.join(',')
   end
   
   def time_per_state_legend_list(values)
-    raw values.map{|a| "\"#{a[0]} (%%.%%)\"" if a[1] > 0 && !['won', 'lost', 'hold'].include?(a[0])}.compact.join(',')
+    raw values.map{|a| "\"#{a[0]} (%%.%%)\"" if a[1].to_f > 0 && !['won', 'lost', 'hold'].include?(a[0])}.compact.join(',')
   end
 end
